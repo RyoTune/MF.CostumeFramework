9c4497c999af2fefd05e814ba97a8e2115a5b47f
RyoTune <zutomayofan50@gmail.com>
2024-10-11 02:53:51 -0500

    Update for release.

--- MF.CostumeFramework.Reloaded/Hooks/AssetHooks.cs
+++ MF.CostumeFramework.Reloaded/Hooks/AssetHooks.cs
@@ -2,13 +2,12 @@
 using MF.CostumeFramework.Reloaded.Costumes.Models;
 using MF.Toolkit.Interfaces.Library;
 using Reloaded.Hooks.Definitions;
-using System.Runtime.InteropServices;
 
 namespace MF.CostumeFramework.Reloaded.Hooks;
 
 internal class AssetHooks
 {
-    private delegate nint GetAsset(nint param1, nint param2, AssetType assetType, int majorId, int minorId, int subId);
+    private delegate nint GetAsset(nint param1, nint param2, AssetType assetType, int majorId, int minorId, int subId, int param7);
     private IHook<GetAsset>? getAssetHook;
     private IHook<GetAsset>? getTexHook;
 
@@ -23,15 +22,15 @@ internal class AssetHooks
         ScanHooks.Add(
             nameof(GetAsset),
             "48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 48 83 EC 70 41 8B D9",
-            (hooks, result) => this.getAssetHook = hooks.CreateHook<GetAsset>((a, b, c, d, e, f) => this.GetAssetImpl(a, b, c, d, e, f, false), result).Activate());
+            (hooks, result) => this.getAssetHook = hooks.CreateHook<GetAsset>((a, b, c, d, e, f, g) => this.GetAssetImpl(a, b, c, d, e, f, g, false), result).Activate());
 
         ScanHooks.Add(
             $"{nameof(GetAsset)}: TEX",
             "48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 48 83 EC 30 41 8B D9 41 8B F0",
-            (hooks, result) => this.getTexHook = hooks.CreateHook<GetAsset>((a, b, c, d, e, f) => this.GetAssetImpl(a, b, c, d, e, f, true), result).Activate());
+            (hooks, result) => this.getTexHook = hooks.CreateHook<GetAsset>((a, b, c, d, e, f, g) => this.GetAssetImpl(a, b, c, d, e, f, g, true), result).Activate());
     }
 
-    private nint GetAssetImpl(nint param1, nint param2, AssetType assetType, int majorId, int minorId, int subId, bool isTex)
+    private nint GetAssetImpl(nint param1, nint param2, AssetType assetType, int majorId, int minorId, int subId, int param7, bool isTex)
     {
         if (assetType == AssetType.CharacterGfs_B && majorId == 1)
         {
@@ -41,10 +40,12 @@ internal class AssetHooks
                 minorId = costume.CostumeId;
                 Log.Information($"Using Costume || {Character.Player} || Costume: {costume.Name}");
             }
+
+            Log.Information($"{nameof(GetAssetImpl)} || {majorId} || {minorId} || {subId}");
         }
 
-        var result = isTex ? this.getTexHook!.OriginalFunction(param1, param2, assetType, majorId, minorId, subId)
-            : this.getAssetHook!.OriginalFunction(param1, param2, assetType, majorId, minorId, subId);
+        var result = isTex ? this.getTexHook!.OriginalFunction(param1, param2, assetType, majorId, minorId, subId, param7)
+            : this.getAssetHook!.OriginalFunction(param1, param2, assetType, majorId, minorId, subId, param7);
 
         return result;
     }
